c97a1b9cd5bbc2e494fd8d58abc660b328e34db8
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-05-04 07:44:07 +0200

    Make the layout

---
+++ src/com/customify/ui/Main.java
@@ -0,0 +1,9 @@
+package com.customify.ui;
+
+import com.customify.ui.layout.Layout;
+
+public class Main {
+    public static void main(String[] args) {
+        new Layout();
+    }
+}
---
+++ src/com/customify/ui/components/Sidebar.java
@@ -0,0 +1,28 @@
+package com.customify.ui.components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Sidebar extends JPanel {
+    public Sidebar(){
+        this.setBackground(new Color(53,32,88));
+        this.setBounds(0,0,300, 1080);
+        this.setLayout(null);
+
+        JPanel logo = this.logo();
+        this.add(logo);
+    }
+
+    public JPanel logo(){
+        JPanel myLogo = new JPanel();
+        myLogo.setBackground(new Color(53,32,88));
+        myLogo.setBounds(53, 81, 200, 100);
+
+        JLabel logoName = new JLabel("Customify");
+        logoName.setFont(new Font("Montserrat", Font.BOLD, 29));
+        logoName.setForeground(new Color(164, 166, 179));
+
+        myLogo.add(logoName);
+        return myLogo;
+    }
+}
