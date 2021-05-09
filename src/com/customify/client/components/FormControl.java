5dbc7056af0d89ccfd32c4a0fb2b5231491603b3
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-05-09 13:16:36 +0200

    Finsh the FormControl

--- src/com/customify/client/components/FormControl.java
+++ src/com/customify/client/components/FormControl.java
@@ -1,11 +1,25 @@
 package com.customify.client.components;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 
 public class FormControl extends JPanel {
-    public FormControl(){
-        JLabel placeholder = new JLabel("Placeholder ");
+    public FormControl(String placeholderTextParam){
+        JLabel placeholderText = new JLabel(placeholderTextParam);
+        placeholderText.setFont(new Font("Montserrat", Font.PLAIN, 18));
+        placeholderText.setBackground(Color.green);
+        placeholderText.setPreferredSize(new Dimension(200, 30));
 
-        add(placeholder);
+
+        JTextField textField = new JTextField("", 20);
+        textField.setBorder(BorderFactory.createCompoundBorder(
+                new LineBorder(Color.black, 1, true),
+                BorderFactory.createEmptyBorder(8, 15, 8, 15))
+                );
+        textField.setFont(new Font("Montserrat", Font.PLAIN, 18));
+
+        add(placeholderText);
+        add(textField);
     }
 }
