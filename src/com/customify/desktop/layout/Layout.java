70285b166af787d719d745b9018f4c06d92fde9f
sandberg-smith <sandbergjacques500@gmail.com>
2021-06-02 08:56:57 +0200

    added user name on header

--- src/com/customify/desktop/layout/Layout.java
+++ src/com/customify/desktop/layout/Layout.java
@@ -1,5 +1,6 @@
 package com.customify.desktop.layout;
 
+import com.customify.desktop.components.SideBarListItem;
 import com.customify.desktop.components.Sidebar;
 
 import javax.swing.*;
@@ -12,6 +13,24 @@ public class Layout  {
         frame.setBackground(Color.white);
         JPanel sidebar = new Sidebar();
         JPanel navbar = new JPanel();
+
+
+        navbar.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+//        JButton profile = new SideBarListItem("profile.png","");
+//        profile.setSize(20, 20);
+//        profile.setMaximumSize(profile.getSize());
+//        navbar.add(profile);
+
+        JLabel name = new JLabel("Gisa Kaze Fredson");;
+        name.setForeground(Color.white);
+        name.setVerticalAlignment(SwingConstants.CENTER);
+        name.setSize(150,100);
+        name.setFont(name.getFont().deriveFont(16.0f));
+
+        navbar.add(name,gbc);
+
         navbar.setBackground(new Color(53,32,88));
         navbar.setBounds(300, 0, 1620, 70);
         body.setBackground(Color.GREEN);
