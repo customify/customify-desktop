fafbd63bc6f9c05017c511ac8beb537cd5f988eb
Iradukunda tamara <tamarameme05@gmail.com>
2021-06-02 21:17:03 +0200

    product gui furnish

--- src/com/customify/desktop/product/NewProduct.java
+++ src/com/customify/desktop/product/NewProduct.java
@@ -1,7 +1,6 @@
-package com.customify.desktop.products;
+package com.customify.desktop.product;
 
 import com.customify.desktop.components.FormControl;
-import com.customify.desktop.components.ProductControl;
 
 import javax.swing.*;
 import javax.swing.border.LineBorder;
@@ -10,38 +9,47 @@ import java.awt.*;
 public class NewProduct extends JPanel {
 
     public NewProduct(){
+
+        JPanel header = new JPanel();
+        //header.setSize(new Dimension(300,30));
+        //header.setBackground(Color.white);
+
+        JLabel headline = new JLabel("ADD PRODUCT ");
+        headline.setFont(new Font("Montserrat", Font.BOLD, 20));
+        headline.setForeground(new Color(53,32,88));
+
+        header.setBackground(Color.white);
+        header.setBounds(15,20,300,30);
+        header.add(headline);
+
         JPanel main = new JPanel();
         main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
         main.setBackground(Color.white);
         setLayout(null);
-
+        main.setBorder(BorderFactory.createCompoundBorder(
+                new LineBorder(Color.black, 1, true),
+                BorderFactory.createEmptyBorder(8, 15, 8, 15))
+        );
         JPanel main2=new JPanel();
         main2.setLayout(new GridLayout(4,2));
         main2.setBackground(Color.white);
-        JPanel header = new JPanel();
-        header.setSize(new Dimension(500,300));
-        JLabel headline = new JLabel("ADD PRODUCT ");
-        //headline.setPreferredSize(new Dimension());
-        headline.setFont(new Font("Montserrat", Font.BOLD, 29));
-        headline.setForeground(new Color(53,32,88));
-        headline.setBounds(-400, 100,800,40);
-        header.setBackground(Color.white);
 
-        JPanel productName = new ProductControl("Product name",200);
-        JPanel productDescription = new ProductControl("Product Description",300);
-        JPanel quantity = new ProductControl("Quantity",200);
-        JPanel points = new ProductControl("Points",200);
-        JPanel price = new ProductControl("Price",200);
+
+        JPanel productName = new FormControl("Product name");
+        JPanel productDescription = new FormControl("Product Description");
+        JPanel quantity = new FormControl("Quantity");
+        JPanel points = new FormControl("Points");
+        JPanel price = new FormControl("Price");
 
         JPanel buttonGroup = new JPanel();
         buttonGroup.setBackground(Color.white);
 
         JButton cancel = new JButton("Cancel");
-        cancel.setBounds(400,400,180,40);
+        cancel.setBounds(400,400,100,20);
         cancel.setBackground(Color.white);
         cancel.setBorder(BorderFactory.createCompoundBorder(
                 cancel.getBorder(),
-                BorderFactory.createEmptyBorder(7, 30, 7, 30)));
+                BorderFactory.createEmptyBorder(7, 90, 2, 90)));
         cancel.setFont(new Font("Montserrat", Font.PLAIN, 18));
 
 
@@ -51,15 +59,15 @@ public class NewProduct extends JPanel {
         btn.setForeground(Color.white);
         btn.setBorder(BorderFactory.createCompoundBorder(
                 btn.getBorder(),
-                BorderFactory.createEmptyBorder(7, 30, 7, 30)));
+                BorderFactory.createEmptyBorder(7, 90, 2, 90)));
         btn.setFont(new Font("Montserrat", Font.PLAIN, 18));
 
         buttonGroup.add(btn);
         buttonGroup.add(cancel);
 
-        header.add(headline);
 
-        main.add(header);
+
+        //main.add(header);
         main2.add(productName);
         main2.add(quantity);
         main2.add(productDescription);
@@ -69,8 +77,9 @@ public class NewProduct extends JPanel {
         main.add(buttonGroup);
 
 
-        main.setBounds(90, 50, 800, 700);
-
+        main.setBounds(90, 50, 800, 600);
+        main2.setBounds(90, 50, 800, 600);
+        add(header);
         add(main);
         setBackground(Color.white);
     }
