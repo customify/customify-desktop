cc2e76c219ba441a05190067045850650aa1646d
gershomlapaix <gershomlapaix>
2021-06-03 19:10:40 +0200

    ending customer feedback

--- src/com/customify/desktop/InitializeConnection.java
+++ src/com/customify/desktop/InitializeConnection.java
@@ -1,8 +1,6 @@
 package com.customify.desktop;
 
 
-import com.customify.cli.services.CustomerFeedbackService;
-import com.customify.desktop.business.NewBusiness;
 import com.customify.desktop.customerFeedback.Businesses;
 import com.customify.desktop.customerFeedback.CustomerFeedbackForm;
 import com.customify.desktop.layout.Layout;
@@ -29,12 +27,8 @@ public class InitializeConnection {
 //            new Layout(new NewBusiness(socket), "New business");
             new Layout(new CustomerFeedbackForm(socket),"Customer feedback registration");
 
-//            String businesses[];
             Businesses b = new Businesses(socket);
-//            b.viewAll();
-//            CustomerFeedbackService c = new CustomerFeedbackService(socket);
-//            System.out.println("Business: "+c.handleGetResponse());
-//            System.out.println(b.viewAll().get(0));
+            System.out.println(b.viewAll().getIds());
 
         } catch (Exception e) {
             System.out.println("Failed to connect to the server at port: " + portNumber);
--- src/com/customify/desktop/utils/interfaces/SelectBusinessFormat.java
+++ src/com/customify/desktop/utils/interfaces/SelectBusinessFormat.java
@@ -1,2 +1,33 @@
-package com.customify.desktop.utils.interfaces;public class SelectBusinessFormat {
+package com.customify.desktop.utils.interfaces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectBusinessFormat {
+    List<Integer> ids = new ArrayList<>();
+    List<String> names = new ArrayList<>();
+
+    public SelectBusinessFormat() {
+    }
+
+    public SelectBusinessFormat(List<Integer> ids, List<String> names) {
+        this.ids = ids;
+        this.names = names;
+    }
+
+    public List<Integer> getIds() {
+        return ids;
+    }
+
+    public void setIds(List<Integer> ids) {
+        this.ids = ids;
+    }
+
+    public List<String> getNames() {
+        return names;
+    }
+
+    public void setNames(List<String> names) {
+        this.names = names;
+    }
 }
