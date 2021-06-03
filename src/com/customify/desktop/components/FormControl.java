c6711f5c62a0e17672c0827d2a5128dc1885a23e
Iradukunda tamara <tamarameme05@gmail.com>
2021-06-03 19:10:34 +0200

    form changed bg color

--- src/com/customify/desktop/components/FormControl.java
+++ src/com/customify/desktop/components/FormControl.java
@@ -5,13 +5,14 @@ import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class FormControl extends JPanel {
+
     public FormControl(String placeholderTextParam){
         setBackground(Color.white);
         JLabel placeholderText = new JLabel(placeholderTextParam);
         placeholderText.setFont(new Font("Montserrat", Font.PLAIN, 18));
-        placeholderText.setBackground(Color.green);
+        placeholderText.setBackground(Color.white);
         placeholderText.setPreferredSize(new Dimension(200, 30));
-
+        placeholderText.setBounds(2,30,100,200);
 
         JTextField textField = new JTextField("", 20);
         textField.setBorder(BorderFactory.createCompoundBorder(
@@ -19,8 +20,10 @@ public class FormControl extends JPanel {
                 BorderFactory.createEmptyBorder(8, 15, 8, 15))
         );
         textField.setFont(new Font("Montserrat", Font.PLAIN, 18));
+        textField.setBounds(2,3,100,200);
 
         add(placeholderText);
         add(textField);
+        setPreferredSize(new Dimension(100,500));
     }
 }
